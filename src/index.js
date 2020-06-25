d8a3ac165948123b0ce5f0c4eb89a0ca1441b062
avi <abhi.uk96@gmail.com>
2020-06-25 21:43:35 +0100

    [intermediate commit] solution to find heavy ball with minimum no of  comparisons

--- src/index.js
+++ src/index.js
@@ -64,13 +64,105 @@ function findDictinaryWord (){
         alert("Please enter hello value");
     }
 
-    let result = typeof(d1[word]) === 'string'? d1[word]: "Result not found";
+    let output = typeof(d1[word]) === 'string'? d1[word]: "Result not found";
     let resultContainer = document.getElementsByClassName("dictionaryResults")[0];
     resultContainer.innerHTML = '';
     
     let span = document.createElement("span");
-    span.innerText = result;
+    span.innerText = output;
     span.classList.add("dictionary-word");
     resultContainer.append(span);
      
+}
+
+
+
+//there are multiple identical balls but one is heavier... with minimum usage of scale find the 
+//heaviest ball
+//without using Math.max(1, 3, 2);
+function findHeavyBall (){
+    let arrBalls = [1, 2, 1, 1, 1, 1, 1, 1], temp = [], sum =[], scaleUsageCount = 0, groupby;
+    let sumLeft = 0, sumRight = 0;
+    //lets get the count of array elements
+    console.log(arrBalls.length);   
+    j=arrBalls.length;
+    groupSize = Math.ceil(j / 3);
+    //console.log(groupSize);
+    // now divide array in three groups
+
+    compareValues(arrBalls);
+    console.log(temp);
+    function compareValues (arrBalls) {
+        let k = 0;
+        let v = [];
+        for(i=0;  i<j; i+=groupSize) {
+            temp[k] = arrBalls.slice(i, i+groupSize);
+
+            //compare first two arrays
+            v.push(getWeight(temp[k]));
+            console.log(v);
+            k++;
+        }
+
+        // if(this.scales.getWeight(temp[counter]) == this.scales.getWeight(temp[counter++])) {
+        //     this.compareValues(temp[counter], temp[counter])
+        // } else if(this.scales.getWeight(temp[counter]) < this.scales.getWeight(temp[counter++])){
+
+        // }
+        // else if(this.scales.getWeight(temp[counter]) > this.scales.getWeight(temp[counter++])){
+
+        // }
+    }
+
+
+
+    // for(m=0; m < k; m++) {
+    //     scaleUsageCount++;
+    //     //compare first 2
+    //     sum[m] = temp[m].reduce(function(a, b){
+    //              return a + b;
+    //     }, 0);
+    // }
+
+   // console.log(sum);
+
+    // sumLeft = temp[0].reduce(function(a, b){
+    //     return a + b;
+    // }, 0);
+    // sumRight = temp[1].reduce(function(a, b){
+    //     return a + b;
+    // }, 0);
+
+    // console.log(sumLeft);
+    // console.log(sumRight);
+
+    function getWeight(arr) {
+        arr.reduce(function(a, b){
+                 return a + b;
+         }, 0);
+    }
+
+
+
+    //compare sum of each group
+    //get the heavier group and again repeat the process
+}
+
+// Here are two sets of digital scales. Their displays are both broken in the same place, so the bottom-left part of a number does not show up. 
+//For example, the number '6' would appear as '5'. The sugar being weighed is showing up one gram heavier than it is on the left-hand scales, 
+// and one gram lighter than it is on the right-hand scales. What is the actual total number of grams of the sugar on both sets of scales?
+
+function digitalScale(){
+    //ds1, ds2
+    //Work out which numbers would not display properly due to the broken section.
+    
+}
+
+
+
+function williamsMaxHeap()
+{
+    //provided an array find tge
+
+
 }
